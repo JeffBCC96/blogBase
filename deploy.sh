287089caece5af4ecd9d94c919f5883067e766f1
rma13 <rong.ma2@mail.mcgill.ca>
2020-07-04 00:39:55 -0400

    fifth

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,23 @@ if [ $# -lt  1 ]; then
     echo "$0 <commit message>"
     exit 1
 fi
+
 msg="$1"
+
+git add . 
 git commit -m "$msg"
 if [ $? -ne 0 ]; then
     echo "Commit failed"
     exit 1
 fi
+
 git push origin master
 if [ $? -ne 0 ]; then
     echo "Push failed"
 fi
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
-hugo -t noteworthy # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 # Go To Public folder
 cd public
 # Add changes to git.
